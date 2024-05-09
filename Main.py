f9c5f6831c671eab17b10b4b55152bb9d956a71b
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-05-08 19:33:03 -0600

    Realizacion de la funcion del algoritmo de djk y funcion para mostrar las rutas menos costosas a un destino seleccionado por el usuario

--- Main.py
+++ Main.py
@@ -5,19 +5,15 @@ import heapq
 rutas = {}
 with open('rutas.txt', 'r') as file:
     for line in file:
-        # Limpiar las comillas dobles de la línea
         line = line.replace('"', '')
         salida, destino, costo = line.strip().split(', ')
         costo = int(costo)
-        # Agregar rutas con costo
-        # Se toma en cuenta para ir de X a Y, y de Y a X
         rutas.setdefault(salida, {})[destino] = costo
         rutas.setdefault(destino, {})[salida] = costo
 
 print("Las rutas establecidas son: ")
 print(rutas)
 
-#Generar grafos en base a las rutas
 G = nx.Graph()
 
 for estacion in rutas.keys():
@@ -26,11 +22,38 @@ for salida, destinos in rutas.items():
     for destino, costo in destinos.items():
         G.add_edge(salida, destino, weight=costo)
 
-nx.draw(G,with_labels=True,node_color="purple",node_size=300)
+nx.draw(G, with_labels=True, node_color="purple", node_size=300)
 plt.margins(0.2)
 
+
 def djk(rutas, inicio):
-    distancias = {nodo:float('inf') for nodo in rutas}
+    distancias = {nodo: float('inf') for nodo in rutas}
     rutas_completadas = {nodo: [] for nodo in rutas}
-    distancias[inicio]=0
+    distancias[inicio] = 0
     priority_Queue = [(0, inicio)]
+
+    while priority_Queue:
+        distancia_actual, node_actual = heapq.heappop(priority_Queue)
+
+        for vecino, costo_De_Vecino in rutas[node_actual].items:
+            distancia_actualizada = distancia_actual + costo_De_Vecino
+
+            if distancia_actualizada < distancias[vecino]:
+                distancias[vecino] = distancia_actualizada
+                rutas_completadas[vecino] = rutas_completadas[node_actual] + [node_actual]
+                heapq.heappush(priority_Queue, (distancia_actualizada, vecino))
+
+    return distancias, rutas_completadas
+
+
+def mostrar_rutas_y_costos(rutas, inicio):
+    distancias, rutas_completas = djk(rutas, inicio)
+
+    print(f"\nDesde '{inicio}':")
+    for destino, costo in distancias.items():
+        if destino != inicio:
+            ruta = rutas_completas[destino] + [destino]
+            if costo < float('inf'):
+                print(f"  - Hasta '{destino}': Ruta: {ruta}, Costo: {costo}")
+            else:
+                print(f"  - Hasta '{destino}': Ruta no encontrada")
